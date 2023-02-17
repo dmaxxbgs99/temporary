843f01dd493bbac25eee367fc1a0e3027d114768
manish <manishg.ias@gmail.com>
2023-02-17 08:08:37 +0530

    first commit

---
+++ age.f
@@ -0,0 +1,12 @@
+let age=15;
+if (age>=21) {
+    console.log('youcan vote')
+} else if(age>=15) {
+    console.log('you are bhoot')
+}
+else if('age<=10') {
+    console.log('ab tak chhappan')
+}
+else if(age<12) {
+  console.log('i am groot')
+}
